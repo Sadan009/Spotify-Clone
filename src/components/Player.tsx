398ebcad3d16a4b368e5cc4f17f8d00c774bbeaa
Sadan009 <imamsadan009@gmail.com>
2025-02-05 13:51:36 +0530

    play pause audio using expo-AV

--- src/components/Player.tsx
+++ src/components/Player.tsx
@@ -2,10 +2,63 @@ import { View, Text, StyleSheet, Image } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 import { tracks } from "../../assets/data/tracks";
 import { usePlayerContext } from "../providers/PlayerProvider";
-
+import { useEffect, useState } from "react";
+import { Audio, AVPlaybackStatus } from "expo-av";
+import { setIsEnabledAsync, Sound } from "expo-av/build/Audio";
 
 const Player = () => {
+  const [sound, setSound] = useState<Sound>();
+  const [isPlaying, setIsPlaying] = useState(false);
   const { track } = usePlayerContext();
+
+  useEffect(() => {
+    if (track) {
+      playTrack();
+    }
+  }, [track]);
+
+  useEffect(() => {
+    return sound
+      ? () => {
+          console.log("Unloading Sound");
+          sound.unloadAsync();
+        }
+      : undefined;
+  }, [sound]);
+
+  const playTrack = async () => {
+    if (sound) {
+      await sound.unloadAsync();
+    }
+
+    if (!track?.preview_url) {
+      return;
+    }
+    const { sound: newSound } = await Audio.Sound.createAsync({
+      uri: track?.preview_url,
+    });
+    setSound(newSound);
+    newSound.setOnPlaybackStatusUpdate(onPlayBackStatusUpdate);
+    await newSound.playAsync();
+  };
+
+  const onPlayBackStatusUpdate = (status: AVPlaybackStatus) => {
+    console.log(status);
+    if (!status.isLoaded) {
+      return;
+    }
+    setIsPlaying(status.isPlaying);
+  };
+
+  const onPlayPause = async () => {
+    if (!sound) return;
+
+    if (isPlaying) {
+      await sound.pauseAsync();
+    }else{
+        await sound.playAsync();
+    }
+  };
 
   if (!track) {
     return null;
@@ -30,8 +83,9 @@ const Player = () => {
           style={{ marginHorizontal: 10 }}
         />
         <Ionicons
+          onPress={onPlayPause}
           disabled={!track?.preview_url}
-          name={"play"}
+          name={isPlaying ? "pause" : "play"}
           size={22}
           color={track?.preview_url ? "white" : "gray"}
         />
